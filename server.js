747dc271e8b00fc75aabf61e2812e03ce7a05e56
jeness <yuhaoran9968@gmail.com>
2018-05-03 16:35:19 -0400

    add static file

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/',(req,res)=>res.send('Hello World!'))
+
 app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
 });
