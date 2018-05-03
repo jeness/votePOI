930b1bf49aba5d33c3580e520f81b6d92188fb43
jeness <yuhaoran9968@gmail.com>
2018-05-02 23:38:28 -0400

    add react route-server:gulp&npm run watch at backend

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+var swig  = require('swig');
+var React = require('react');
+var Router = require('react-router');
+var routes = require('./app/routes');
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
@@ -11,7 +15,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/',(req,res)=>res.send('Hello World!'))
+// app.get('/',(req,res)=>res.send('Hello World!'))
+
+app.use(function(req, res) {
+    Router.run(routes, req.path, function(Handler) {
+      var html = React.renderToString(React.createElement(Handler));
+      var page = swig.renderFile('views/index.html', { html: html });
+      res.send(page);
+    });
+  });
+
 
 app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
